e3127549a2bd619583112989d6bf4b33b56128aa
Shivam Gupta <shivamg727583@.com>
2024-06-10 15:18:10 +0530

    updated

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ const { checkForAuthenticationCookie } = require('./middleware/authentication');
 
 const port = process.env.PORT || 3000;
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+
+
+
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+    })
     .then(() => {
-        console.log("Connected to database");
+      console.log("Connected to database");
     })
     .catch((err) => {
-        console.log("Not connected to database", err);
+      console.error("Database connection error:", err);
     });
 
 // Set view engine to EJS
